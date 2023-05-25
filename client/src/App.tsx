c6cda91b2b4d2e7dc1889691a8f5d120c8d29028
Kirill Starodubsev <colereteps@gmail.com>
2023-05-26 00:10:16 +0400

    chore: Add basic configuration for client

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const App: FC = (): JSX.Element => {
+    return <div>App</div>;
+};
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const { webpack } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, './src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        clean: true,
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 4000,
+        open: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.json', '.js'],
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true,
+                    },
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.wooff2?$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ],
+    },
+
+};
+
