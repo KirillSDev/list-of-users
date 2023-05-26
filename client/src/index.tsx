871b473031e9411fa152bcec0477b491ca05d101
Kirill Starodubsev <colereteps@gmail.com>
2023-05-26 15:36:47 +0400

    feat(style): Add bootstrap

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<App />);
