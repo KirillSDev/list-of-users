80819db56ef3f5d227ed0a181f358fd5d74d6256
Kirill Starodubsev <colereteps@gmail.com>
2023-05-28 00:12:41 +0400

    feat: Add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import { FC } from 'react';
+import { Home } from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import { UsersPage } from './pages/UsersPage';
+import './styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: FC = (): JSX.Element => {
-    return <div>App</div>;
+    return (
+        <Routes>
+            <Route path={'/'} element={<Home />} />
+            <Route path={'/login'} element={<Home />} />
+            <Route path={'/register'} element={<Home />} />
+            <Route path={'/users'} element={<UsersPage />} />
+        </Routes>
+    );
 };
 
 export default App;
