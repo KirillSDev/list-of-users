f46ac3678f55a67cf40dc4db10dcefe932e38088
Kirill Starodubsev <colereteps@gmail.com>
2023-05-29 13:05:40 +0400

    feat: Add UserPage

---
+++ client/src/pages/UsersPage.tsx
@@ -0,0 +1,68 @@
+import { ChangeEvent, FC } from 'react';
+import { Layout } from '../layout/Layout';
+import { UserTable } from '@components/UserTable/UserTable';
+import { Button } from '@components/Button/Button';
+import { Stack } from 'react-bootstrap';
+import { IUser } from '@interfaces/User.interface';
+import { observer } from 'mobx-react';
+import users from '@store/usersStore';
+import DeleteIcon from '@assets/icons/delete.svg';
+import UnblockIcon from '@assets/icons/unblock.svg';
+
+export const UsersPage: FC = observer((): JSX.Element => {
+    const handleCheckboxClick = (e: ChangeEvent<HTMLInputElement>, user: IUser) => {
+        if (e.target.checked) {
+            users.selectUser(user.id);
+        } else {
+            users.removeFromSelectedUser(user.id);
+        }
+    };
+    const selectAll = (e: ChangeEvent<HTMLInputElement>) => {
+        const isChecked = e.target.checked;
+        if (isChecked) {
+            users.users.forEach((user) => {
+                if (!users.selectedUsers.includes(user.id)) {
+                    users.selectedUsers.push(user.id);
+                }
+            });
+        } else {
+            users.clearSelectedUsers();
+        }
+    };
+    return (
+        <Layout>
+            <Stack direction={'horizontal'} className='my-3 d-flex justify-content-center gap-2'>
+                <Button
+                    onClick={() => {
+                        users.blockUsers();
+                    }}
+                >
+                    Block
+                </Button>
+                <Button
+                    image={UnblockIcon}
+                    onClick={() => {
+                        users.unblockUsers();
+                    }}
+                >
+                    Unblock
+                </Button>
+                <Button
+                    image={DeleteIcon}
+                    onClick={() => {
+                        users.deleteUsers();
+                    }}
+                >
+                    Delete
+                </Button>
+            </Stack>
+            <UserTable
+                selectAll={selectAll}
+                selectedUsers={users.selectedUsers}
+                clearSelectedUsers={users.clearSelectedUsers}
+                onCheckboxClick={handleCheckboxClick}
+                users={users.users}
+            ></UserTable>
+        </Layout>
+    );
+});
