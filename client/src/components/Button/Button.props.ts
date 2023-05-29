382e74cfdd9bd7adab0383e334ad9e852cec0327
Kirill Starodubsev <colereteps@gmail.com>
2023-05-29 12:47:02 +0400

    feat: Add Button Icon Feature

--- client/src/components/Button/Button.props.ts
+++ client/src/components/Button/Button.props.ts
@@ -1,6 +1,7 @@
-import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, ReactNode, SVGProps } from 'react';
 
 export interface IButton extends DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     children: ReactNode;
     apperance?: 'primary' | 'unprimary';
+    image?: string;
 }
--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,18 +1,19 @@
 import { FC } from 'react';
 import { IButton } from './Button.props';
-import { Col, Row } from 'react-bootstrap';
+
 import styles from './Button.module.scss';
 import cn from 'classnames';
-export const Button: FC<IButton> = ({ children, apperance = 'primary', ...props }): JSX.Element => {
+export const Button: FC<IButton> = ({ image, children, apperance = 'primary', ...props }): JSX.Element => {
     return (
         <button
-            className={cn(styles.button, {
+            className={cn(styles.button, 'd-flex justify-content-center gap-1 align-items-center', {
                 ['bg-warning']: apperance === 'primary',
                 ['bg-black text-white']: apperance === 'unprimary',
             })}
             {...props}
         >
             {children}
+            {image && <img src={image} alt='icon' height={20}></img>}
         </button>
     );
 };
