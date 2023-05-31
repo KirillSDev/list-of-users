48c0fb7ab32a19f8c97f3490c366dafa73478e31
Kirill Starodubsev <colereteps@gmail.com>
2023-05-31 17:52:08 +0400

    feat: Add AuthService

---
+++ client/src/services/AuthService.ts
@@ -0,0 +1,36 @@
+import { loginUser } from '@api/Auth/loginUser';
+import { registerUser } from '@api/Auth/registerUser';
+import { validateToken } from '@api/Auth/validateToken';
+import { IFormData } from '@interfaces/FormData.interface';
+import { authStore } from '@store/AuthStore';
+
+export class AuthService {
+    static async loginUser(formData: IFormData) {
+        try {
+            authStore.setLoading();
+            const data = await loginUser(formData);
+            authStore.login(data.token);
+        } catch (error: any) {
+            authStore.setMessage(error.response.data.error);
+        }
+    }
+    static async registerUser(formData: IFormData) {
+        try {
+            authStore.setLoading();
+            const data = await registerUser(formData);
+            authStore.setMessage(data);
+        } catch (error: any) {
+            authStore.setMessage(error.response.data.error);
+        }
+    }
+    static async validateToken() {
+        try {
+            const response = await validateToken();
+            authStore.setStatusAuthenticated(true);
+        } catch (error: any) {
+            if (error.response && error.response.status === 401) {
+                authStore.setStatusAuthenticated(false);
+            }
+        }
+    }
+}
