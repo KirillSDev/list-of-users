9d2faa19c8f0a85227dff8930e30cb6f7fc79046
Kirill Starodubsev <colereteps@gmail.com>
2023-05-31 18:59:42 +0400

    chore: Add dotenv

--- server/database/index.ts
+++ server/database/index.ts
@@ -1,7 +1,7 @@
 import { dbConfig } from '../config/db-config';
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(dbConfig.DATABASE, dbConfig.USER, dbConfig.PASSWORD, {
+export const sequelize = new Sequelize(dbConfig.DATABASE!, dbConfig.USER!, dbConfig.PASSWORD!, {
     host: dbConfig.HOST,
     dialect: 'mysql',
 });
