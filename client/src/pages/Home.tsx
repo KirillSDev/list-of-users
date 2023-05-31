e75b3d923c4d283b17827ff53326c04f1db0dc92
Kirill Starodubsev <colereteps@gmail.com>
2023-05-31 18:11:48 +0400

    feat: Add check for authorization and logic for working with the modal

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,32 @@
-import { FormGroup } from '@components/FormGroup/FormGroup';
 import { Layout } from '../layout/Layout';
 import { FC, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { ModalComponent } from '@components/Modal/Modal';
+import { authStore } from '@store/AuthStore';
+import { observer } from 'mobx-react';
+import { Loading } from '@components/Loading/Loading';
 
-export const Home: FC = (): JSX.Element => {
-    const location = useLocation();
-    const [typeForm, setTypeForm] = useState<string>('login');
+export const Home: FC = observer((): JSX.Element => {
+    const navigate = useNavigate();
+    const [show, setShow] = useState<boolean>(false);
+    const closeModal = () => {
+        setShow(false);
+        authStore.clearMessage();
+    };
     useEffect(() => {
-        setTypeForm(location.pathname.slice(1));
-    }, [location]);
+        if (authStore.message) {
+            setShow(true);
+        }
+        if (authStore.isAuthenticated) {
+            navigate('/users');
+        }
+    }, [authStore.message, authStore.isAuthenticated]);
+
     return (
         <Layout>
-            <FormGroup type={typeForm === 'login' ? 'Login' : 'Register'}></FormGroup>
+            <Loading status={authStore.loading} />
+            <Outlet />
+            <ModalComponent message={authStore.message} show={show} closeModal={closeModal}></ModalComponent>
         </Layout>
     );
-};
+});
